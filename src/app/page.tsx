d6642fbaf60a2ea8dd522ca656a58279c7d27b3d
almeidadylan <almeidadylan793@gmail.com>
2023-01-26 11:24:21 -0400

    initial commmit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Teste from "./page-test.tsx"
+
+const App = () => {
+
+  
+  return (
+    <div id="fullpage">
+      <Teste />
+    </div>
+  );
+};
+
+export default App;
